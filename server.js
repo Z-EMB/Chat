16845e3b3d4711813a73b3af514e2d8080be639c
Bernami <emarkbarnett@gmail.com>
2016-01-27 21:53:59 -0500

    The most important commit

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
+var favicon = require('serve-favicon');
 var app = express();
 
+app.use(favicon(__dirname+'/public/resources/favicon.png'));
 app.get('/*', function (req, res) {
     res.send('Hello World!');
 });
 
-app.listen(3000);
-console.log('Listening on port 3000');
+app.listen(3001);
+console.log('Listening on port 3001');
 
