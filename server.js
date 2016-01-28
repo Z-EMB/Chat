839765ce47370429902bd43d93c981df1db561a4
Bernami <emarkbarnett@gmail.com>
2016-01-27 19:44:20 -0500

    Structure

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
 
-var server = app.listen(3000, function() {
-    console.log('Listening on port 3000');
+app.get('/*', function (req, res) {
+    res.send('Hello World!');
 });
+
+app.listen(3000);
+console.log('Listening on port 3000');
 
