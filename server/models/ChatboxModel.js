40d02aadb78c9e2edbaefe5abdb42727e11e685f
Felipe Spinolo <fspinol0@gmail.com>
2016-02-06 08:52:51 -0500

    basic chatbox model

--- server/models/ChatboxModel.js
+++ server/models/ChatboxModel.js
@@ -1,3 +1,27 @@
 /**
- * Created by Z on 1/27/2016.
+ * Defintion of chat object
  */
+
+module.exports = function() {
+    var Chat = function(chatID) {
+        this.chatID = chatID;
+        this.log = this.getLog();
+    };
+
+    Chat.prototype.getLog = function() {
+        return (this.log || []);
+    };
+
+    Chat.prototype.message = function(usr, msg) {
+        this.log.push({
+            uid: usr,
+            msg: msg
+        });
+
+        if (this.log.length > 100) {
+            this.log.shift();
+        };
+    };
+
+    return Chat;
+};
