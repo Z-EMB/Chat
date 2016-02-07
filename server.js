a9ecf513eadf05a49d1bd5943f0a0b7ba412e1e6
Felipe Spinolo <fspinol0@gmail.com>
2016-02-06 22:31:12 -0500

    folder globals

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-global.rootDirectory = __dirname;
+global.root = __dirname;
+global.backend = global.root + '/server';
+global.models = global.backend + '/models';
+global.controllers = global.backend + '/controllers';
+global.views = global.backend + '/views';
+global.config = global.backend + '/config';
 
 var express = require('express');
-var settings = require('./server/config/ExternalSettings.js');
+var settings = require(global.config + '/ExternalSettings.js');
 
 var app = express();
 var PORT = 8080;
@@ -9,7 +14,7 @@ var PORT = 8080;
 settings(app,"favicon");
 
 // routing
-require('./server/controllers/main.js')(app, express);
+require(global.controllers + '/main.js')(app, express);
 
 var server = app.listen(PORT, '0.0.0.0', function() {
     console.log('Chat listening on port ' + PORT);
