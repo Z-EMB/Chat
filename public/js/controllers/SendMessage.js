a005e0f2b5962c8e8148834780d6ffadb564fa0b
Bernami <emarkbarnett@gmail.com>
2016-02-11 15:54:19 -0500

    chatwindow lobby

--- public/js/controllers/SendMessage.js
+++ public/js/controllers/SendMessage.js
@@ -1,1 +1,13 @@
+$(document).ready(function() {
+    $('.sendButton').click(function() {
+        //post ajax here with content from messageTextInput
 
+    });
+
+    $('.messageTextInput').keyup(function(event){
+        if(event.keyCode == 13){
+            $('.sendButton').click();
+        }
+    });
+
+});
