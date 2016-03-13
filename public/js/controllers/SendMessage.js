40e9fd49b94a67561f518e8b76cd8ec95a78ac07
Felipe Spinolo <fspinol0@gmail.com>
2016-03-13 15:41:59 -0400

    minor tweaks for frontend

--- public/js/controllers/SendMessage.js
+++ public/js/controllers/SendMessage.js
@@ -1,28 +1,13 @@
 $(document).ready(function() {
+    var socket = io();
 
     function getMessageValue(){
         return $('.messageTextInput').val();
     }
 
-    function buildJSONStringRequest(){
-        //will add user here for now default
-        return JSON.stringify({requestValue:{user:'default',message:getMessageValue()}});
-    }
-
     $('.sendButton').click(function() {
-//        var jsonData = buildJSONStringRequest();
-        //post ajax here with content from messageTextInput
-   //     $.ajax({
-     //       method: 'POST',
-       //     url: '/message',
-     //       contentType:'application/json',
-       //     data: jsonData
-     //   }).done(function(res){
-       //     console.log('post success,value: ' + JSON.stringify(res));
-    var socket = io();
-    socket.emit('sendMessage', getMessageValue());
-        }); 
-
+        socket.emit('sendMessage', getMessageValue());
+    });
 
     $('.messageTextInput').keyup(function(event){
         if(event.keyCode == 13){
