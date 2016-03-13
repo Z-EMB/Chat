3bb723ea7cd6847ded3fed7560f56c016af4463c
Mike Gwyn <mikegwyn17@gmail.com>
2016-03-13 16:09:05 -0400

    added updateChat

--- public/js/controllers/SendMessage.js
+++ public/js/controllers/SendMessage.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
     var socket = io();
+    socket.on('updateChat', function(username, message) {
+        $('.chatWindow').append("<p> username: " + username +
+            "Message: " + message + "</p>" );
+        console.log('I have recieved the update event');
+    });
 
     function getMessageValue(){
         return $('.messageTextInput').val();
@@ -8,6 +13,7 @@ $(document).ready(function() {
     $('.sendButton').click(function() {
         socket.emit('sendMessage', getMessageValue());
     });
+
 
     $('.messageTextInput').keyup(function(event){
         if(event.keyCode == 13){
