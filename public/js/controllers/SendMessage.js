abe63c45c98b38ee434fbd2d8338f79f3340b9bf
Mike Gwyn <mikegwyn17@gmail.com>
2016-03-16 11:43:14 -0400

    working on the popup

--- public/js/controllers/SendMessage.js
+++ public/js/controllers/SendMessage.js
@@ -1,11 +1,47 @@
 $(document).ready(function() {
-    var socket = io();
-//
-    socket.emit('userConnect');
+    var chatWindow = $('.chatWindow');
 
+    $('.errorMsg').hide();
+    var id = '#LoginBoxContent';
+    // set the width and height of the mask to cover the whole screen 
+    var maskHeight = $(document).height();
+    var maskWidth = $(window).width();
+    var mask = '#mask'; 
+    $(mask).css({'width':maskWidth, 'height':maskHeight});
+    $(mask).fadeIn(500);
+    $(mask).fadeTo("slow",0.9);
+    
+    // gethe window width and height and set the popup to center in the window
+    var windowHeight = $(window).height();
+    var windowWidth = $(window).width();
+    $(id).css('top', windowHeight/2-$(id).height()/2);
+    $(id).css('left', windowWidth/2-$(id).width()/2);
+    $(id).fadeIn(2000);
+
+    // once the button is clicked
+    $('.loginButton').click(function () {
+        var username = $('.login_username').val();
+        var chatroom = $('.login_chatroom').val();
+        $(mask).hide();
+        $('.window').hide();
+        var socket = io();
+        socket.emit('userConnect',username,chatroom);
+
+       // $('.errorMsg').show();
+    });
+
+    
     socket.on('updateChat', function(username, message) {
-        $('.chatWindow').append("<p> username: " + username +
-            "Message: " + message + "</p>" );
+        if (username === 'EMF_HOST' ) {
+            chatWindow.append("<p><b> username: " + username +
+                " Message: " + message + "</b></p>" );
+        }
+        else {
+           chatWindow.append("<p> username: " + username +
+                " Message: " + message + "</p>" );
+        }
+        var height = chatWindow[0].scrollHeight;
+        chatWindow.scrollTop(height);
         console.log('I have received the update event');
     });
 
@@ -32,9 +68,10 @@ $(document).ready(function() {
         if(getMessageValue()){
             socket.emit('sendMessage', getMessageValue());
             $('.messageTextInput').val("");
+            var height = chatWindow[0].scrollHeight;
+            chatWindow.scrollTop(height);
         }
     });
-
 
     $('.messageTextInput').keyup(function(event){
         if(event.keyCode == 13){
